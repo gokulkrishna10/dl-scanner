a7ccabfaf44ac19ef5b6f1ef503161aa854232a6
gokul_krishna <gokulkrishnagk62@gmail.com>
2024-02-06 22:57:52 -0500

    added project skeleton

---
+++ dao/dlScannerDAO.js
@@ -0,0 +1,1 @@
+const db = require('../customnodemodules/database_node_module/app')
---
+++ validation/dlScannerValidation.js
@@ -0,0 +1,8 @@
+const util = require('../customnodemodules/util_node_module/utils')
+const ErrorMod = require('../customnodemodules/error_node_module/errors')
+const customError = new ErrorMod()
+
+
+
+
+
